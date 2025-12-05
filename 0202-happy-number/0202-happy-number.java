4e3982913dd87f0dab57e136a865aaf1662f03f3
Nnandakishor <147476605+Nnandakishor@users.noreply.github.com>
2025-12-05 11:28:21 +0530

    Time: 1 ms (75.16%), Space: 42.5 MB (15.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = next(n);
+        while (fast != 1 && slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        return fast == 1;
+    }
+    private int next(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
